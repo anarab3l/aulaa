327ac641fa969662188b4b0302949485433e8042
anarab3l <ana.rabel@escola.pr.gov.br>
2024-10-10 11:52:00 -0300

    dfgdsg

---
+++ perguntas.js
@@ -0,0 +1,100 @@
+criaCartao(
+    'Português',
+    'Quem é o escritor de Memórias Póstumas de Brás Cubas',
+    'Machado de Assis')
+    
+    criaCartao(
+    'Biologia',
+    'Como é o nome das células filtradoras do filo Porifera',
+    'Coanócitos')
+    
+    criaCartao(
+    'Sociologia',
+    'Ano da morte de Max Weber',
+    '1920')
+    
+    criaCartao(
+    'História',
+    'Quais os países que se aliaram em 1936, que tem com sigla "RoBerTo" ',
+    'Roma, Berlim, Tokyo')
+    
+    criaCartao(
+    'História',
+    'Quais os estados que faziam parte da Política Café com leite?',
+    'Minas Gerais e São Paulo')
+    
+    criaCartao(
+    'Português',
+    'Quais os tipos de Romantismo?',
+    'Ramance urbano, romance indianista, romance histórico e romance regionalista')
+    
+    criaCartao(
+    'Português',
+    'Uma característica presente nos livros de Machado de Assis é a',
+    'Ironia')
+    
+    criaCartao(
+    'História',
+    ' Por qual motivo a família real portuguesa deixa Portugal?',
+    ' Fugindo de Napoleão')
+    
+    criaCartao(
+    'História',
+    'Em que ano a família real portuguesa foge de Portugal?',
+    '1808')
+    
+    criaCartao(
+    'Geografia',
+    ' Exemplo de um fator Climático',
+    'Relevo')
+    
+    criaCartao(
+    'História',
+    'Por que o governo de Getúlio Vargas se encerrou em 1954?',
+    'O ex presidente tirou a própria vida')
+    
+    criaCartao(
+    'Matemática',
+    'Qual sempre será o resultado da equação sen x2+cos x2?',
+    '1') 
+    
+    criaCartao(
+    'Geografia',
+    'Qual o modelo de produção difundido na Segunda Revolução Industrial?',
+    'Taylorismo e Fordismo')
+    
+    criaCartao(
+    'Química',
+    'Qual o sufixo do álcool na química orgânica',
+    'ol')
+    
+    criaCartao(
+    'Biologia',
+    'Quem é considerado o pai da genética?',
+    'Gregor Mendel')
+    
+    criaCartao(
+    'Inglês',
+    'Qual o comparativo de beautiful?',
+    'more beautiful')
+    
+    criaCartao(
+    'Sociologia',
+    'O termo luta de classes foi proposto por qual sociólogo?',
+    'Karl Marx')
+    
+    criaCartao(
+    'Biologia',
+    'O que é mimetismo?',
+    'Quando um indivíduo imita o outro para obter vantagens sobre isso, como proteção, acasalamento ou confundir a presa')
+    
+    criaCartao(
+    'Matemática',
+    'Qual a fórmula proposta por Pitágoras?',
+    'a2=b2+c2')
+    
+    criaCartao(
+    'História',
+    'Verdadeiro ou falso, a ditadura militar no Brasil teve apenas um presidente',
+    'Falso, o período de ditadura militar contou com cinco presidentes')
+    
