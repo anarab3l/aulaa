15bb263546ccd0e5edcbf28ca96875bfc999e2d2
anarab3l <ana.rabel@escola.pr.gov.br>
2024-10-30 07:48:00 -0300

    bn

--- main.js
+++ main.js
@@ -14,13 +14,14 @@ function criaCartao(categoria, pergunta, resposta) {
 </div>
 
 `
-    let respostaEstaVisivel = false;
+let respostaEstaVisivel = false;
 
-    function viraCartao (){
+function viraCartao (){
         respostaEstaVisivel = !respostaEstaVisivel;
         cartao.classList.toggle('active', respostaEstaVisivel);
     }
 
-    cartao.addEventListener('click', viraCartao);
- container.appendChild(cartao);
+cartao.addEventListener('click', viraCartao);
+
+container.appendChild(cartao);
 }
