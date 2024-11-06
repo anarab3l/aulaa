d4cb9081d01245e7f2897b6c3c0a350bc24c71b2
anarab3l <ana.rabel@escola.pr.gov.br>
2024-11-06 07:34:34 -0300

    ryry

--- pagina2.js
+++ pagina2.js
@@ -16,3 +16,12 @@ function criaCartao(categoria, pergunta, resposta) {
   
   container.appendChild(cartao);
 }
+function mostaResposta{
+    const resposta= card.querySelector('.resposta-cartao'),
+    if (resposta.style.display==='none') {
+        resposta.style.display='block';
+    }
+}
+else{
+    reposta.style.display = 'none';
+}
