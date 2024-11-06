43fdf717324675e8cc0ce3b3a8f360b4c95eda24
anarab3l <ana.rabel@escola.pr.gov.br>
2024-11-06 07:36:29 -0300

    ffg

--- pagina2.js
+++ pagina2.js
@@ -16,9 +16,9 @@ function criaCartao(categoria, pergunta, resposta) {
   
   container.appendChild(cartao);
 }
-function mostaResposta{
-    const resposta= card.querySelector('.resposta-cartao'),
-    if (resposta.style.display==='none') {
+function mostaResposta (card) {
+    const resposta= card.querySelector ('.resposta-cartao'),
+    if (resposta.style.display==='none'){
         resposta.style.display='block';
     }
 }
