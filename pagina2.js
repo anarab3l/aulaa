be6a9693ccb835ecef1360db4be4a70b2a89b999
anarab3l <ana.rabel@escola.pr.gov.br>
2024-11-06 07:45:55 -0300

    gjgj

--- pagina2.js
+++ pagina2.js
@@ -2,8 +2,8 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
+    onclick "mostrarResposta(this)"
     cartao.innerHTML = `
-      onclick= "mostrarResposta(this)"
     <div class="conteudo-cartao">
        <h3>${categoria}</h3>
        <div class="pergunta-cartao">
